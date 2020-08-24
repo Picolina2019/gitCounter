90916a3243a700de779774c2d73f1739f94a33ae
 Katia S <katesakowicz@MacBook-Pro-4.local>
2020-08-24 21:42:36 +0200

    Updates

---
+++ precache-manifest.ce8c161e854d8eda2b52b003ab2004c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367931cd5c6dd86085ffa4560a67cc5c",
+    "url": "/gitCounter/index.html"
+  },
+  {
+    "revision": "466e12d54267763ad0bb",
+    "url": "/gitCounter/static/css/main.191d9704.chunk.css"
+  },
+  {
+    "revision": "f5bcb864a766ed87a72f",
+    "url": "/gitCounter/static/js/2.ce2e68af.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gitCounter/static/js/2.ce2e68af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "466e12d54267763ad0bb",
+    "url": "/gitCounter/static/js/main.e75727d1.chunk.js"
+  },
+  {
+    "revision": "b5ef3e86a9a73de72252",
+    "url": "/gitCounter/static/js/runtime-main.d5fb282b.js"
+  }
+]);
